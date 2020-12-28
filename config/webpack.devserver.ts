f6ed6af4f0d259b963e4e01f867136c1269ee9af
Josef Reichelt <pepareichelt@gmail.com>
2020-12-28 15:10:29 +0100

    Add host option to webpack devserver

--- config/webpack.devserver.ts
+++ config/webpack.devserver.ts
@@ -3,6 +3,7 @@ import { BUILD_PATH } from './webpack.config.base';
 
 export const devServerConfig: Configuration = {
     port: 1337,
+    host: '0.0.0.0',
     contentBase: [ '../src/', '../dist/' ],
     stats: { modules: false },
     hot: true,
