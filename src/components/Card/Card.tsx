5ada79fe1f2f89398f6e8ba4b6983a511f4a989f
Josef Reichelt <pepareichelt@gmail.com>
2021-01-10 22:11:30 +0100

    Add card hover effect

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import css from './Card.css';
 import { getCardDataCoordinates } from './utils';
+import CardBack from '_Assets/card-back.svg';
 
 export interface ICardProps {
     onClick?: (cardId:number) => void;
@@ -15,10 +16,42 @@ export interface ICardProps {
 export const Card = ({ sizeX = 128, sizeY = 128, isFlipped, isHidden, cardName, cardId, onClick }: ICardProps) => {
     const { x, y } = getCardDataCoordinates(cardName);
     const posX = isFlipped ? sizeX : 0;
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+
+    }, []);
+
+    const calculateCardTranfsorm = (mouseX:number, mouseY:number) => {
+        if (!ref.current) return '';
+        const boundingBox = ref.current.getBoundingClientRect();
+        const rotationLimit = 4;
+        const rotateY = (mouseX - boundingBox.x - (boundingBox.width / 2)) / rotationLimit;
+        const rotateX = (mouseY - boundingBox.y - (boundingBox.height / 2)) / rotationLimit;
+
+        return `perspective(100px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(0.75)`;
+    };
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        requestAnimationFrame(()=>{
+            if (!ref.current) return;
+            ref.current.style.transform = calculateCardTranfsorm(e.clientX, e.clientY);
+        });
+    };
+
+    const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        requestAnimationFrame(()=>{
+            if (!ref.current) return;
+            ref.current.style.transform = 'perspective(100px) rotateX(0deg) rotateY(0deg) scale(0.75)';
+        });
+    };
+
+
 
     return (
         <div
             data-cardname={cardName}
+            ref={ref}
             style={{
                 backgroundPositionX: -posX,
                 width: sizeX,
@@ -28,6 +61,8 @@ export const Card = ({ sizeX = 128, sizeY = 128, isFlipped, isHidden, cardName, 
             }}
             className={css.card}
             onClick={()=> {onClick && onClick(cardId);} }
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
         >
             {isFlipped && (
                 <div style={{
