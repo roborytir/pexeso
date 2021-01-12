923af0b9f0d975ab5aa33e9af133e78f3f36c726
Josef Reichelt <pepareichelt@gmail.com>
2021-01-12 15:42:53 +0100

    Add card fliping transition

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import css from './Card.css';
 import { getCardDataCoordinates } from './utils';
-import CardBack from '_Assets/card-back.svg';
+import CardBack from '_Assets/card-back.svg?sprite';
 
 export interface ICardProps {
     onClick?: (cardId:number) => void;
@@ -17,32 +17,34 @@ export const Card = ({ sizeX = 128, sizeY = 128, isFlipped, isHidden, cardName, 
     const { x, y } = getCardDataCoordinates(cardName);
     const posX = isFlipped ? sizeX : 0;
     const ref = useRef<HTMLDivElement>(null);
+    const flippedRotation = isFlipped ? -180 : 0;
+    const defaultRotation = `perspective(100px) rotateX(0deg) rotateY(${flippedRotation}deg) scale(0.75)`;
 
     useEffect(()=>{
 
     }, []);
 
+    console.log(isFlipped);
     const calculateCardTranfsorm = (mouseX:number, mouseY:number) => {
         if (!ref.current) return '';
         const boundingBox = ref.current.getBoundingClientRect();
         const rotationLimit = 4;
         const rotateY = (mouseX - boundingBox.x - (boundingBox.width / 2)) / rotationLimit;
         const rotateX = (mouseY - boundingBox.y - (boundingBox.height / 2)) / rotationLimit;
-
-        return `perspective(100px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(0.75)`;
+        return `perspective(100px) rotateX(${-rotateX}deg) rotateY(${rotateY + flippedRotation}deg) scale(0.75)`;
     };
 
-    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         requestAnimationFrame(()=>{
             if (!ref.current) return;
             ref.current.style.transform = calculateCardTranfsorm(e.clientX, e.clientY);
         });
     };
 
-    const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const handleMouseLeave = () => {
         requestAnimationFrame(()=>{
             if (!ref.current) return;
-            ref.current.style.transform = 'perspective(100px) rotateX(0deg) rotateY(0deg) scale(0.75)';
+            ref.current.style.transform = defaultRotation;
         });
     };
 
@@ -57,7 +59,8 @@ export const Card = ({ sizeX = 128, sizeY = 128, isFlipped, isHidden, cardName, 
                 width: sizeX,
                 height: sizeY,
                 opacity: isHidden ? 0 : 1,
-                pointerEvents: (isHidden || isFlipped) ? 'none' : 'all'
+                transform: defaultRotation,
+                pointerEvents: (isHidden) ? 'none' : 'all'
             }}
             className={css.card}
             onClick={()=> {onClick && onClick(cardId);} }
@@ -66,8 +69,12 @@ export const Card = ({ sizeX = 128, sizeY = 128, isFlipped, isHidden, cardName, 
         >
             {isFlipped && (
                 <div style={{
-                    backgroundPositionX: -x, backgroundPositionY: -y, width: sizeX, height: sizeY
-                }} className={css.cardSymbol} />
+                    backgroundPositionX: -x,
+                    backgroundPositionY: -y,
+                    width: sizeX,
+                    height: sizeY,
+                }} className={css.cardSymbol}
+                />
             )}
         </div>
     );
